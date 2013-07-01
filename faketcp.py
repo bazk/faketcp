f95682b8d546a41d069891a3776ec3e376cae9e4
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2013-07-01 19:11:16 -0300

    Add Packet

--- faketcp.py
+++ faketcp.py
@@ -16,10 +16,11 @@
 # along with faketcp. If not, see <http://www.gnu.org/licenses/>.
 
 import socket
+import struct
 
 class Socket(object):
     def __init__(self):
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def connect(self, address):
         self._socket.connect(address)
@@ -28,7 +29,8 @@ class Socket(object):
         self._socket.bind(address)
 
     def listen(self, backlog):
-        self._socket.listen(backlog)
+        data, addr = self._socket.recvfrom(BUFSIZ)
+        packet = Packet.from_data(data)
 
     def accept(self):
         conn = Connection(self._socket.accept())
@@ -65,3 +67,47 @@ class Connection(object):
 
     def close(self):
         self._conn.close()
+
+
+class Packet(object):
+    def __init__(self):
+        self.seq = 0
+        self.ack = 0
+        self.flags = 0
+        self.win = 0
+        self.checksum = 0
+        self.payload = ''
+
+    def __str__(self):
+        return str((self.seq, self.ack, self.flags, \
+            self.win, self.checksum, self.payload))
+
+    @staticmethod
+    def from_data(data):
+        packet = Packet()
+
+        packet.seq, packet.ack, packet.flags, packet.win, \
+          packet.checksum = struct.unpack('!IIHHH', data[0:14])
+        packet.payload = data[14:]
+
+        if packet.calculate_checksum(data[0:14]) != 0:
+            raise Exception('checksum failed')
+
+        return packet
+
+    def to_data(self):
+        header = struct.pack('!IIHH', self.seq, self.ack, self.flags, self.win)
+        checksum = struct.pack('!H', self.calculate_checksum(header))
+        return header + checksum + self.payload
+
+    def calculate_checksum(self, data):
+        def carry_around_add(a, b):
+            c = a + b
+            return (c & 0xffff) + (c >> 16)
+
+        checksum = 0x0000
+
+        for word in struct.unpack('!' + 'H' * (len(data) / 2), data):
+            checksum = carry_around_add(checksum, word)
+
+        return (checksum ^ 0xffff)
